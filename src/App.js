3d3aeb4b8781982ae1c493f3149dc500f8e8f5f1
ashaburle <ashaburle06@gmail.com>
2024-03-09 13:53:32 +0000

    commit message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import RestaurantPage from './RestaurantPage'
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <RestaurantPage />
+    </div>
+  )
+}
+
+export default App
